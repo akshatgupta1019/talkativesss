c34744badfafd6206f50eba947487b0295bf9a28
akshatgupta1019 <50566056+akshatgupta1019@users.noreply.github.com>
2021-10-08 09:32:10 +0530

    Add files via upload

---
+++ project_chat_application/client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    <div>
+      <h1>Welcome to talkatives<span role="img" aria-label="emoji">💬</span></h1>
+      <h2>Created with<span role="img" aria-label="emoji">❤️</span></h2>
+      <h2>by Akshat Gupta</h2>
+    </div>
+    {
+      users
+        ? (
+          <div>
+            <h1>People currently chatting:</h1>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                    <img alt="Online Icon" src={onlineIcon}/>
+                  </div>
+                ))}
+              </h2>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
